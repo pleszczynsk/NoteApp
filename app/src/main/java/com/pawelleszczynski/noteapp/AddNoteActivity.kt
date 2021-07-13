84ee5ef5a956da9c89a4c5b3ae26e7e429b1d29e
GhostIntheAfro <pleszczyns@gmail.com>
2021-07-13 16:09:59 +0200

    initial commit

---
+++ app/src/main/java/com/pawelleszczynski/noteapp/AddNoteActivity.kt
@@ -0,0 +1,60 @@
+package com.pawelleszczynski.noteapp
+
+import android.content.ContentValues
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_add_note.*
+
+class AddNoteActivity : AppCompatActivity() {
+
+    var id = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+        try {
+            val bundle:Bundle = intent.extras!!
+            id = bundle.getInt("ID", 0)
+            if (id!=0){
+                titleEt.setText(bundle.getString("name"))
+                descEt.setText(bundle.getString("des"))
+            }
+        }
+        catch (ex:Exception){}
+    }
+
+    fun addFunc(view:View)
+    {
+        var dbManager = DbManager(this)
+        var values = ContentValues()
+        values.put("Title", titleEt.text.toString())
+        values.put("Description", descEt.text.toString())
+
+        if (id ==0){
+            val ID = dbManager.insert(values)
+            if (ID>0){
+                Toast.makeText(this, "Note Successfully added!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            else
+            {
+                Toast.makeText(this, "Error: Could not add note!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else
+        {
+            var selectionArgs = arrayOf(id.toString())
+            val ID = dbManager.update(values, "ID=?", selectionArgs)
+            if (ID>0){
+                Toast.makeText(this, "Note Successfully added!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            else
+            {
+                Toast.makeText(this, "Error: Could not add note!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/pawelleszczynski/noteapp/Note.kt
@@ -0,0 +1,9 @@
+package com.pawelleszczynski.noteapp
+
+class Note(nodeID: Int, nodeName: String, nodeDes: String) {
+
+    var nodeID: Int? = nodeID
+    var nodeName: String? = nodeName
+    var nodeDes: String? = nodeDes
+
+}
